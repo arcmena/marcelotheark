60c5a5f29ec5cef5736eab9c300d3562bd933d17
arcmena <marcelomena010@gmail.com>
2021-11-09 16:41:16 -0300

    chore: remove unused style

--- components/layouts/HomePage/TimelineLayout/styles.ts
+++ components/layouts/HomePage/TimelineLayout/styles.ts
@@ -5,7 +5,6 @@ export const TimelineContainer = styled.section(() => css``)
 export const TimelineListWrapper = styled.ul(
   ({ theme: { spacing } }) => css`
     margin-top: ${spacing[8]};
-    margin-bottom: 2;
 
     > li:not(:first-child) {
       margin-top: ${spacing[6]};
