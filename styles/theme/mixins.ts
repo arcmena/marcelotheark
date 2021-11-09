995ea165938121c954b7f3aafaaf2712420dacfc
arcmena <marcelomena010@gmail.com>
2021-11-09 17:28:03 -0300

    refactor: change border-gradient to png

--- styles/theme/mixins.ts
+++ styles/theme/mixins.ts
@@ -4,7 +4,11 @@ const mixins = Object.freeze({
   primaryGradient: 'background: linear-gradient(to right, #6417CB, #74C6FA);',
   textPrimaryGradient:
     'background: linear-gradient(to right, #6417CB, #74C6FA); background: -webkit-gradient(linear, left middle, left middle, from(#6417cb), to(#74c6fa) ); background-clip: text; -webkit-text-fill-color: transparent; -webkit-background-clip: text;',
-  borderPrimaryGradient: `border-style: solid; border-image: url(${GRADIENTS.BORDER_GRADIENT}); border-image-slice: 2;`
+  borderPrimaryGradient: `
+    border-image: url(${GRADIENTS.BORDER_GRADIENT}); 
+    border-image-slice: 3; 
+    border-style: solid; 
+  `
 })
 
 export { mixins }
