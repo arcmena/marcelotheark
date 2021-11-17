92622ec6fc5218b1cc235be9592d2a0c7fb7d015
arcmena <marcelomena010@gmail.com>
2021-11-17 15:07:50 -0300

    fix: getFullDate not showing the day

--- helpers/dateHelpers.ts
+++ helpers/dateHelpers.ts
@@ -3,4 +3,4 @@ import { format } from 'date-fns'
 export const getMonthAndDay = (date: string) => format(new Date(date), 'MMMM d')
 
 export const getFullDate = (date: string) =>
-  format(new Date(date), 'MMMM 4, yyyy')
+  format(new Date(date), 'MMMM d, yyyy')
