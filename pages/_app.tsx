03f0c0d605bf1b14c3becea1cde24d3d6ba5e64f
arcmena <marcelomena010@gmail.com>
2021-11-17 20:59:24 -0300

    feat: add ga tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,40 @@
+import { useEffect } from 'hoist-non-react-statics/node_modules/@types/react'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
 import { ThemeProvider } from 'styled-components'
 
 import Layout from '../components/common/Layout'
 
+import * as gtag from '../lib/gtag'
+
 import GlobalStyles from '../styles/global'
 import theme from '../styles/theme'
 
+declare global {
+  interface Window {
+    gtag: any
+  }
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageView(url)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
+
       <Layout>
         <Component {...pageProps} />
       </Layout>
