3fe4ebe421ee73e340edc3d26bb27eeeb56d4316
arcmena <marcelomena010@gmail.com>
2021-12-01 19:42:11 -0300

    feat: implement next-sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteUrl: 'https://marcelotheark.com',
+  generateRobotsTxt: true,
+  exclude: ['/server-sitemap.xml'],
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      'https://marcelotheark.com/server-sitemap.xml' // <==== Add here
+    ]
+  }
+}
