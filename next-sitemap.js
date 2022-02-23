6ace7171b62a9f74b816970357958f71ba337001
Marcelo Mena <marcelomena010@gmail.com>
2022-02-23 14:25:06 -0300

    sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteUrl: 'https://marcelotheark.com',
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    additionalSitemaps: ['https://marcelotheark.com/server-sitemap.xml']
+  }
+}
