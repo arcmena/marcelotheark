1c45d9eaa31988324501d73d9ea5f5d57b78dacd
Marcelo Mena <marcelomena010@gmail.com>
2022-02-23 14:50:43 -0300

    wip

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteUrl: 'https://marcelotheark.com',
   generateRobotsTxt: true,
+  exclude: ['/server-sitemap.xml'],
   robotsTxtOptions: {
     additionalSitemaps: ['https://marcelotheark.com/server-sitemap.xml']
   }
