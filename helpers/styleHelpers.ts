7b0e4c9e786c69fb4bbbeeb1269248246719e893
Marcelo Mena <marcelomena010@gmail.com>
2022-03-15 20:06:41 -0300

    feat: create style helper margin and padding

--- helpers/styleHelpers.ts
+++ helpers/styleHelpers.ts
@@ -18,3 +18,23 @@ export const gridLayout = ({
   grid-template-columns: repeat(${columns}, minmax(0, 1fr));
   gap: ${gap};
 `
+
+export const marginY = (size: string) => css`
+  margin-top: ${size};
+  margin-bottom: ${size};
+`
+
+export const marginZ = (size: string) => css`
+  margin-left: ${size};
+  margin-right: ${size};
+`
+
+export const paddingY = (size: string) => css`
+  padding-top: ${size};
+  padding-bottom: ${size};
+`
+
+export const paddingZ = (size: string) => css`
+  padding-left: ${size};
+  padding-right: ${size};
+`
