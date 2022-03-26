335d5c0809d784e2a7b3eb6a379f1317537753b1
Marcelo Mena <marcelomena010@gmail.com>
2022-03-26 17:23:06 -0300

    chore: font optimization

--- components/common/GoogleFonts/GoogleFonts.tsx
+++ components/common/GoogleFonts/GoogleFonts.tsx
@@ -3,7 +3,7 @@
 export default function GoogleFonts() {
   return (
     <link
-      href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,400;0,500;1,500&display=swap"
+      href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,400;0,500;1,500&display=optional"
       rel="stylesheet"
     />
   )
