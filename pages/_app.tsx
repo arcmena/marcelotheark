1154ea477fb8c62d9f197553ca3388b5845b8aad
Marcelo Mena <marcelomena010@gmail.com>
2022-03-27 20:57:39 -0300

    chore: gtag optimization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,10 @@
 import { useEffect } from 'react'
 import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
+import Script from 'next/script'
 import { ThemeProvider } from 'styled-components'
 
 import Layout from '@components/common/Layout'
-
-import GoogleAnalytics from '@components/common/GoogleAnalytics/GoogleAnalytics'
 
 import * as gtag from '@lib/gtag'
 
@@ -35,7 +34,24 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <GoogleAnalytics />
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+        }}
+      />
 
       <ThemeProvider theme={theme}>
         <GlobalStyles />
